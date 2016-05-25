db155df45bebe690a5aab36bdff2db6b650f5a9d
transplanar <transplanar@gmail.com>
2016-05-24 23:58:47 -0400

    Styling for modal and home view

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,12 +1,14 @@
 (function(){
   function ModalCtrl($scope, $log, $uibModal){
     var size = 'sm';
+//    var size = 100;
     
     $scope.open = function(size){
       var modalInstance = $uibModal.open({
         templateUrl: 'templates/modalContent.html',
         controller: 'ModalInstanceCtrl',
-        size: size
+        size: size,
+        windowClass: 'modal-window'
       });
       
       modalInstance.result.then(
